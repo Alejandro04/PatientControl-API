e42680a0508b154b30023dc540fb0cbb31d606f8
Alejandro Roa <alejandro.roa@workiasolutions.com>
2024-05-05 17:55:56 -0400

    delete method

--- src/main/java/com/example/demo/services/HistoryService.java
+++ src/main/java/com/example/demo/services/HistoryService.java
@@ -9,30 +9,13 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class HistoryService {
-    private final EntityManager entityManager;
     private final HistoryRepository historyRepository;
 
-    public HistoryService(HistoryRepository historyRepository, EntityManager entityManager, HistoryRepository historyRepository1){
-        this.entityManager = entityManager;
+    public HistoryService(HistoryRepository historyRepository){
         this.historyRepository = historyRepository;
     }
 
-    @Transactional
-   public ResponseEntity<?> createHistory(History history){
-       String query = "INSERT INTO history (date, reason_consulting, details_consulting, patient_id) " +
-               "VALUES (:date, :reason, :details, :patient_id)";
-
-       entityManager.createNativeQuery(query)
-               .setParameter("date", history.getDate())
-               .setParameter("reason", history.getReason_consulting())
-               .setParameter("details", history.getDetails_consulting())
-               .setParameter("patient_id", 1)
-               .executeUpdate();
-
-       return ResponseEntity.ok(history);
-   }
-
-   public History saveHistory(History history){
+    public History saveHistory(History history){
         return this.historyRepository.save(history);
    }
 }
